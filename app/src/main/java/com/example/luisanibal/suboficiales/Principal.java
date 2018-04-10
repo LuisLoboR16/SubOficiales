2ced1b59079dc7eb78e3c5e7c2c9e76f258d7dfa
Luis <lwolf95@hotmail.com>
2018-04-10 16:08:05 -0500

    GUI TERMINADA V1

--- app/src/main/java/com/example/luisanibal/suboficiales/Principal.java
+++ app/src/main/java/com/example/luisanibal/suboficiales/Principal.java
@@ -1,13 +1,25 @@
 package com.example.luisanibal.suboficiales;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Principal extends AppCompatActivity {
-
+    Button siguiente;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        siguiente = (Button)findViewById(R.id.btnContenido);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguiente = new Intent(Principal.this, Inicio.class);
+                startActivity(siguiente);
+            }
+        });
     }
 }
